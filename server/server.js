217a323a146a96f3331baa58660ed069ec84b3f3
Analista TI <analistati.lvr@gsm.com>
2018-08-24 17:27:37 -0500

    Init commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const path = require('path');
+const http = require('http');
+
+const express = require('express');
+const io = require('socket.io');
+
+const PORT = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '/../public');
+
+let app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(PORT, () => {
+    console.log(`[INFO] Server listening on port ${PORT}`);
+});
