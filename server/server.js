7b4933b1da8a690e5eb25fcddc67ebf1ef5200e4
Analista TI <ca.aram@hotmail.com>
2018-08-25 13:40:42 -0500

    test

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const http = require('http');
 
 const express = require('express');
-const io = require('socket.io');
+const socketIOgit = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '/../public');
@@ -12,7 +12,7 @@ const publicPath = path.join(__dirname, '/../public');
 let app = express();
 
 app.use(express.static(publicPath));
-
+//
 app.listen(PORT, () => {
     console.log(`[INFO] Server listening on port ${PORT}`);
 });
