f129cf7b4956e4d58776cfa59b8d756aa67dca10
Aram <AramC92@users.noreply.github.com>
2018-08-29 16:22:19 -0500

    Init commit

--- server/server.js
+++ server/server.js
@@ -1,18 +1,34 @@
 'use strict';
 
+// native node modules
 const path = require('path');
 const http = require('http');
-
+// external node modules
+const socketIO = require('socket.io');
 const express = require('express');
-const socketIOgit = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '/../public');
 
 let app = express();
+// setup httpserver using express behind the scenes
+let server = http.createServer(app);
+// integrate socketio to the server
+let io = socketIO(server);
 
+// set public path to serve content
 app.use(express.static(publicPath));
-////
-app.listen(PORT, () => {
+
+// add socketio event listeners
+io.on('connection', (socket) => {
+    console.log('[INFO] User connected');
+    // function to run when socket is disconnected
+    socket.on('disconnect', () => {
+        console.log('[INFO] User disconnected');
+    });
+});
+
+// make the server listen to requests on a specified port
+server.listen(PORT, () => {
     console.log(`[INFO] Server listening on port ${PORT}`);
 });
